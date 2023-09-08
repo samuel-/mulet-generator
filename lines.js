1a1a26c6eb6473e122139a5db530439cfc4ea639
Samuel Braikeh <samuel.braikeh@yahoo.fr>
2023-09-08 10:52:06 +0200

    feat. array of array  + add lines

--- lines.js
+++ lines.js
@@ -1,5 +1,27 @@
 const lines = [
-    'a-dada sur mon mulet', 
-    'tristes mèches musardent...', 
-    'chauve qui peut !',
+    [
+        'a-dada sur mon mulet', 
+        'tristes mèches musardent...', 
+        'chauve qui peut !',
+        'amulez le feu !',
+    ],
+    [
+        '1 mulet de perdu, 10 paires de mules de retrouvées',
+        'ainsi vivent la cigale et le mulet',
+        'une certaine amulette',
+    ],
+    [
+        'la suspension hydraulique dérape...',
+        'même sans pendentif',
+        'j\'entends le bruit des sans pou ni loi', 
+    ],
+    [
+        'en sautant du trottoir je me suis cassé le mulet',
+        'un mulet n\'est pas qu\'ne mèche et bien plus qu\'une sauce escabèche',
+        [
+            'nous en avions le mulet tout hérissé',
+            'ce mulet me herisse le poil',
+        ],
+        'les mulets muent lorsque ça ne va plus',
+    ],
 ];
